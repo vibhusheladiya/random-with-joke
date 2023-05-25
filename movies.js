ef89e6770a0c31eeb66764222036bb50128af922
vibhu <vibhusheladiya@170gmail.com>
2023-05-25 11:45:54 +0400

    joke

---
+++ movies.js
@@ -0,0 +1,318 @@
+let myjokes= [ 
+    `What do cats eat for breakfast?Mice Cispies!`,
+
+    `What do you call an elephant that doesn’t matter?
+    An irreephant!`,
+    
+    `What do you get, when you cross a rabbit with shellfish?
+    An oyste bunny!`,
+    
+    `Where do polar ,bears keep their money?
+    In a snw bank!`,
+    
+    `Why did the pon,y get sent to his room?
+    He wouldn’t stop horsingaround!`,
+    
+    `What kind of do,g does a magician have?
+    A Labracad brador!`,
+    
+    `Where do cows g,o on Friday nights?
+    They go to the mo-vies!`,
+    `Why couldn't th,e pony sing “Happy Birthday?”
+    Because she was just a littlehoarse!`,
+    
+    `How do you mak,e an octopus laugh?
+    With ten-ickles!`,
+    
+    `How do you keep, a bull from charging?
+    Take away its credt card!`,
+    
+    `Why can’t a leo,pard hide?
+    Because he’s always spotte!`,
+    
+    
+    `What do frogs orde,r at fast-food restaurants?
+    Frenchflies!`,
+    
+    `What do you get, from a pampered cow?
+    Spoild milk!`,
+    
+    `What do you cal,l an alligator who solves mysteries?
+    An invesigator!`,
+    
+    `Why is a snake ,difficult to fool?
+    You can’t pull ts leg!`,
+    
+    `What kind of so,cks do grizzlies wear?
+    None, they have ber feet!`,
+    
+    `What do you get, when you cross a snail with a porcupine?
+    A sowpoke!`,
+    
+    `What did the do,g say when it sat on sandpaper?Ruff!`,
+    
+    `What's a cat's favorite dessert?
+    Chocolat mouse!`,
+    
+    `What fish only ,swims at night?
+    Sarfish!`,
+    
+    `What does a tri,ceratops sit on?
+    Its tricerabottom!`,
+    
+    `Knock Knock Jok,es for Kids
+    jokes for kids
+    Knock, knock.`,
+    `Who’s t,here?
+    Knoc, knock.`,
+    `Who’s there?
+    Figs.`,
+    `Figs who?
+    Figs the doorbell,, I’ve been knocking forever!`,
+    
+    `Who’s there?
+    Theodore.
+    Theodore who?
+    Theodore wasn’t op,ened so I knocked!`,
+    
+    `Knock, knock.
+    Who’ there?
+    Tank.
+    Tank who?
+    You’re welcome!`,
+    
+    `Knock, knock.
+    ,Who’ there?
+    Isabel.
+    Isabel who?
+    Isabel not working?`,
+    
+    `Knok, knock.
+    ,Who’s there?
+    Ice cream.
+    Ice cream who?
+    ICE CREAM SO, YOU CAN `,
+    
+    `Knock, knock.
+    Who’s there?
+    Icy.
+    Icy who?
+    Icy you in there!`,
+    
+    `Knoc, knock.
+    Who’s there?
+    Dozen.
+    Dozen who?
+    Dozen anyone want ,to let me `,
+    
+    `Knoc, knock.
+    Who’s there?
+    Scold.
+    Scold who?
+    Scold outside, let, me in!`,
+    
+    `Knoc, knock.
+    Who’s there?
+    Water.
+    Water who?
+    Water you asking s,o many questions `,
+    
+    `Kock, knock.
+    Who’s there?
+    Cargo.
+    Cargo who?
+    Car go, “Toot toot,`,
+    
+    `50 best jokes fo kids
+    Knock, knock.
+    Who’s there?
+    Hatch.
+    Hatch who?
+    Bless you!,`,
+    
+    `Knock, knock.
+    Who’ there?
+    Annie.
+    Annie who?
+    Annie body home`,
+    
+    `Knock knock.
+    ,Who’s there?
+    Boo.
+    Boo who?
+    Don’t cry, it’s ju,st a `,
+    
+    `Knoc, knock.
+    Who’s there?
+    Lettuce.
+    Lettuce who?
+    Lettuce in, it’s c,old out `,
+    
+    `Knock, knock.
+    Who’ there?
+    An interrupting cow.
+    An interrupt—
+    MOO!`,
+    
+    `Knock,knock.,
+    Who’ there?
+    Owls go.
+    Owls go who?
+    That’` ,
+    
+    `Knock, kno,ck.
+    Who’s there?
+    A little old lady.
+    A ,little old lady who?`,
+    
+    
+    `Knock, knock.
+    Who’ there?
+    Nana.
+    Nana who?
+    Nana your business!`,
+    
+    `Knock, knock.
+    ,Who’ there?
+    Toodle
+    Toodle who?
+    Toodle-l`,
+    
+    `Knock, knock.
+    ,Who’s there?
+    Spell.
+    Spell who?
+    Oka,y, W-H-O!`,
+    
+    
+    
+    
+    `Food Jokes for Kids
+    best jokes for kids
+    What’s a piece of bre,ad’s least favorite`,
+    
+    `What did the bunny say to the carrot?
+    It’s been nice gnawing `,
+    
+    `What do you cal,l `,
+    
+    `Why did the tom,ato blush?
+    Because it `,
+    
+    `What did the `,
+    
+    `Which vegetable, do `,
+    
+    `What do you cal,l a cheese that’s not yours?
+    Nacho`,
+    
+    `Why did the coo,kie go to the nurse?
+    Because he felt`,
+    
+    `What kind of ro,om doesn’t have doors?
+    A m`,
+    
+    `What kind of ke,y opens a banana?`,
+    
+    
+    `What happens wh,en a grape gets run over `,
+    
+    `okes for kids
+    What does garlic `,
+    
+    `Where does frui,t go on vacation?`,
+    
+    `Why did the mel,ons choose not to get married?Because they can`,
+    
+    `What does a lem,on say when it answers the phone?`,
+    
+    `What did one dried fruit say when another asked it to the movies?`,
+    
+    
+    `Why, do bees hav,e sticky hair?
+    They use hon`,
+    
+    `What does a cow, call an earthquake?
+    A mi`,
+    
+    `Why did the man, go to the yogurt museum?
+    To get a little ulture!`,
+    
+    `Why couldn't th,e sesame seed climb up the hill?
+    Because it was ona roll!`,
+    `Why can't you ,trust tacos?
+    Because they always spill te beans!`,
+    
+    `Math Jokes for ,Kids
+    best jokes for kids
+    How come no one picks, statistics as their favorite subject?
+    It’s just verage.`,
+    
+    `Why did two 4s skip dinner?
+    Because they aleady 8!`,
+    
+    `Did you hear ab,out the mathematician who’s afraid of negative numbers?
+    He’ll stop at nothing to avod them!`,
+    
+    `What did the t,riangle feel sorry for the circle?
+    Because it’s pontless!`,
+    
+    `Why was the mat,h book sad?
+    It had too many problems!`,
+    
+    `Did you hear ab,out the rancher who had 97 cows in his field?When he rounded them up, he ad 100!`,
+    
+    `Why couldn’t the angle get a loan?
+    Because his parents wouldn’tcosine!`,
+    
+    `What do you cal,l a number that can’t stay in one place?
+    A roamin’ umeral!`,
+    
+    `Why should you ,never trust someone writing on graph paper?
+    Because they must be plotting soething!`,
+    
+    `What do you ca,ll two guys who love math?
+    Algebros!`,
+    
+    `What do you get, when you divide a Jack o' lantern by its diameter?
+    Pumkini!`,
+    
+    
+    `50 best jokes for kids
+    What did the 90° a,ngle say after an argument?
+    It turns out, I wasright!`,
+    
+    `What tool do mathematicians use most?
+    Multipliers!`,
+    
+    `Why did the stu,dent get upset when their teacher called them average?
+    It was a mean thingto say!`,
+    
+    `If a math teac,her had four apples in one hand and five apples in the other hand, what would they have altogether?
+    Really bi hands!`,
+    
+    `What did the 0 ,say to the 8?
+    Nie belt!`,
+    
+    `Why was 6 afrai,d of 7?
+    Because , 8, 9!`,
+    
+    `Why should you ,never start a conversation with pi?
+    It'll just go on orever!`,
+    
+    `What is a bird’,s favorite type of math?
+    gebra!`,
+    
+    `Why do teenager,s travel in groups of threes and fives?
+    Because they can’t even!`,
+    
+    `Did you hear ab,out the the circle that kept going to school?`
+]
+
+
+    let index = Math.floor(Math.random() * (myjokes.length - 1))
+    console.log(index);
+    joke.innerHTML = myjokes[index]
+
+
+    
